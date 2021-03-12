11f6fa39a84916922deb09fcbf1bc19fb426c0cb
Jim Szalewski <jim.szalewski@gmail.com>
2021-03-12 10:31:55 -0700

    fixes cafe pluralization and creates cafe scaffold

---
+++ street_cafes/app/views/cafes/_cafe.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! cafe, :id, :created_at, :updated_at
+json.url cafe_url(cafe, format: :json)
---
+++ street_cafes/test/system/cafes_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class CavesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit cafes_url
+  #
+  #   assert_selector "h1", text: "Cafe"
+  # end
+end
